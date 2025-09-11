121ff4dac82e5711718215337f32479a1bfaaa64
kerogs <luckywies@gmail.com>
2025-09-12 01:41:55 +0200

    Add slider with swiper.js, add templates images, fix header styles

--- includes/header.php
+++ includes/header.php
@@ -1,57 +1,81 @@
-    <header>
-        <div class="header__inner">
-            <nav>
-                <ul>
-                    <a href="/">
-                        <li class="logo">
-                            <img src="/assets/img/logo_primary.png" alt="">
-                        </li>
-                    </a>
+<?php
 
-                    <form action="all" class="searchHeader" method="GET">
-                        <div class="group">
-                            <input type="text" name="q" id="q" placeholder="Search...">
-                            <button type="submit">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
-                                    <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
-                                </svg>
-                            </button>
-                        </div>
-                    </form>
-                </ul>
-            </nav>
-            <nav>
-                <ul>
-                    <a href="">
+// Pages with a transparent header and be blur when scrolling
+$shyHeader = ['index.php', 'scan.php'];
+
+/*
+class="<?= in_array(basename($_SERVER['PHP_SELF']), $shyHeader) ? 'header-shy' : ''; ?>"
+*/
+
+?>
+<header class="<?= in_array(basename($_SERVER['PHP_SELF']), $shyHeader) ? 'header-shy' : ''; ?>">
+    <div class="header__inner">
+        <nav>
+            <ul>
+                <a href="/">
+                    <li class="logo">
+                        <img src="/assets/img/logo_primary.png" alt="">
+                    </li>
+                </a>
+
+                <form action="all" class="searchHeader" method="GET">
+                    <div class="group">
+                        <input type="text" name="q" id="q" placeholder="Search...">
+                        <button type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                                <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+                            </svg>
+                        </button>
+                    </div>
+                </form>
+            </ul>
+        </nav>
+        <nav>
+            <ul>
+                <a href="">
+                    <li class="icon_button">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                            <path d="M520-120v-320h320v320H520Zm0-400v-320h320v320H520Zm-400 0v-320h320v320H120Zm0 400v-320h320v320H120Z" />
+                        </svg>
+                    </li>
+                </a>
+                <?php if (!$auth->isLoggedIn()) { ?>
+                    <a href="/login">
                         <li class="icon_button">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
-                                <path d="M520-120v-320h320v320H520Zm0-400v-320h320v320H520Zm-400 0v-320h320v320H120Zm0 400v-320h320v320H120Z" />
+                                <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Z" />
                             </svg>
                         </li>
                     </a>
-                    <?php if (!$auth->isLoggedIn()) { ?>
-                        <a href="/login">
-                            <li class="icon_button">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
-                                    <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Z" />
-                                </svg>
-                            </li>
-                        </a>
-                    <?php } elseif ($auth->isLoggedIn()) { ?>
-                        <a href="/add/scan">
-                            <li class="icon_button">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
-                                    <path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z" />
-                                </svg>
-                            </li>
-                        </a>
-                        <a href="/profile/<?php echo htmlspecialchars($auth->getUser()['id']); ?>">
-                            <li class="profile">
-                                <img src="<?php echo htmlspecialchars($auth->getUser()['profile_picture']); ?>" alt="">
-                            </li>
-                        </a>
-                    <?php } ?>
-                </ul>
-            </nav>
-        </div>
-    </header>
+                <?php } elseif ($auth->isLoggedIn()) { ?>
+                    <a href="/add/scan">
+                        <li class="icon_button">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                                <path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z" />
+                            </svg>
+                        </li>
+                    </a>
+                    <a href="/profile/<?php echo htmlspecialchars($auth->getUser()['id']); ?>">
+                        <li class="profile">
+                            <img src="<?php echo htmlspecialchars($auth->getUser()['profile_picture']); ?>" alt="">
+                        </li>
+                    </a>
+                <?php } ?>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<?php if (in_array(basename($_SERVER['PHP_SELF']), $shyHeader)) { ?>
+    <script>
+        const header = document.querySelector('header');
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 10) {
+                header.classList.add('scrolled');
+            } else {
+                header.classList.remove('scrolled');
+            }
+        });
+    </script>
+<?php } ?>
--- index.php
+++ index.php
@@ -15,11 +15,195 @@ require_once __DIR__ . '/includes/core.php';
     <link rel="stylesheet" href="/assets/styles/css/style.css">
 
     <?php require_once __DIR__ . '/includes/head.php' ?>
+
+    <link rel="stylesheet" href="/node_modules/swiper/swiper-bundle.min.css" />
+    <script src="/node_modules/swiper/swiper-bundle.min.js"></script>
+
 </head>
 
 <body class="dark">
 
     <?php require_once __DIR__ . '/includes/header.php' ?>
+
+    <div class="mainReco-swiper">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide">
+
+
+                <div class="mainReco" style="background-image:url('/assets/img/templates/cover.webp');">
+                    <div class="mainRecoContent">
+                        <div class="filter"></div>
+
+                        <div class="container">
+                            <div class="container__inner">
+                                <div class="cover">
+                                    <a href="">
+                                        <img src="/assets/img/templates/cover.webp" alt="">
+                                    </a>
+                                </div>
+                                <div class="contents">
+                                    <h3 data-swiper-parallax="-100">My bias gets on the last train Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam autem consequatur explicabo minima perspiciatis, blanditiis animi est saepe, tenetur quas, earum nulla cum esse laudantium laborum optio molestias excepturi maiores.</h3>
+                                    <p class="tags">
+                                        <span>Romance</span>
+                                        <span>Drama</span>
+                                        <span>Slice of Life</span>
+                                        <span>Webtoon</span>
+                                    </p>
+                                    <p data-swiper-parallax="-200" class="description">
+                                        “Meeting her on the last train again today, if only I could talk to her!”
+                                        College student Lee Yeowoon works late and takes the last train every night.
+                                        Each time, he runs into Shin Haein, a woman carrying a guitar. As if by chance, as if by fate,
+                                        the two keep meeting and discover that their favorite artist is the indie musician “Long Afternoon.” They gradually grow closer and so the story began. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem neque odio, impedit nostrum illo consequuntur! Exercitationem pariatur repudiandae, nobis sapiente fugiat architecto animi illo amet at optio natus sint eligendi.
+                                    </p>
+                                    <div class="btns">
+                                        <a href="/scan/3" class="btn">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                                                <path d="M240-280h280v-80H240v80Zm400 0h80v-400h-80v400ZM240-440h280v-80H240v80Zm0-160h280v-80H240v80Zm-80 480q-33 0-56.5-23.5T80-200v-560q0-33 23.5-56.5T160-840h640q33 0 56.5 23.5T880-760v560q0 33-23.5 56.5T800-120H160Z" />
+                                            </svg>
+                                            See more
+                                        </a>
+                                        <a href="/scan/3/1" class="btn btn__secondary">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                                                <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm80-160h280v-80H280v80Zm0-160h400v-80H280v80Zm0-160h400v-80H280v80Z" />
+                                            </svg>
+                                            Read now
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="swiper-slide">
+
+                <div class="mainReco" style="background-image:url('/assets/img/templates/cover.webp');">
+                    <div class="mainRecoContent">
+                        <div class="filter"></div>
+
+                        <div class="container">
+                            <div class="container__inner">
+                                <div class="cover">
+                                    <a href="">
+                                        <img src="/assets/img/templates/cover.webp" alt="">
+                                    </a>
+                                </div>
+                                <div class="contents">
+                                    <h3>fezogfjeriugjeiogjreio gets on the last train Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam autem consequatur explicabo minima perspiciatis, blanditiis animi est saepe, tenetur quas, earum nulla cum esse laudantium laborum optio molestias excepturi maiores.</h3>
+                                    <p class="tags">
+                                        <span>Romance</span>
+                                        <span>Drama</span>
+                                        <span>Slice of Life</span>
+                                        <span>Webtoon</span>
+                                    </p>
+                                    <p class="description">
+                                        “Meeting her on the last train again today, if only I could talk to her!”
+                                        College student Lee Yeowoon works late and takes the last train every night.
+                                        Each time, he runs into Shin Haein, a woman carrying a guitar. As if by chance, as if by fate,
+                                        the two keep meeting and discover that their favorite artist is the indie musician “Long Afternoon.” They gradually grow closer and so the story began. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem neque odio, impedit nostrum illo consequuntur! Exercitationem pariatur repudiandae, nobis sapiente fugiat architecto animi illo amet at optio natus sint eligendi.
+                                    </p>
+                                    <div class="btns">
+                                        <a href="/scan/3" class="btn">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                                                <path d="M240-280h280v-80H240v80Zm400 0h80v-400h-80v400ZM240-440h280v-80H240v80Zm0-160h280v-80H240v80Zm-80 480q-33 0-56.5-23.5T80-200v-560q0-33 23.5-56.5T160-840h640q33 0 56.5 23.5T880-760v560q0 33-23.5 56.5T800-120H160Z" />
+                                            </svg>
+                                            See more
+                                        </a>
+                                        <a href="/scan/3/1" class="btn btn__secondary">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                                                <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm80-160h280v-80H280v80Zm0-160h400v-80H280v80Zm0-160h400v-80H280v80Z" />
+                                            </svg>
+                                            Read now
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+    </div>
+
+    <script>
+        const swiper = new Swiper('.mainReco-swiper', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: true,
+            },
+            parallax: true,
+            mousewheel: false,
+            keyboard: true,
+
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    </script>
+
+    <!-- <div class="mainReco" style="background-image:url('/assets/img/templates/cover.webp');">
+        <div class="mainRecoContent">
+            <div class="filter"></div>
+
+            <div class="container">
+                <div class="container__inner">
+                    <div class="cover">
+                        <a href="">
+                            <img src="/assets/img/templates/cover.webp" alt="">
+                        </a>
+                    </div>
+                    <div class="contents">
+                        <h3>My bias gets on the last train Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam autem consequatur explicabo minima perspiciatis, blanditiis animi est saepe, tenetur quas, earum nulla cum esse laudantium laborum optio molestias excepturi maiores.</h3>
+                        <p class="tags">
+                            <span>Romance</span>
+                            <span>Drama</span>
+                            <span>Slice of Life</span>
+                            <span>Webtoon</span>
+                        </p>
+                        <p class="description">
+                            “Meeting her on the last train again today, if only I could talk to her!”
+                            College student Lee Yeowoon works late and takes the last train every night.
+                            Each time, he runs into Shin Haein, a woman carrying a guitar. As if by chance, as if by fate,
+                            the two keep meeting and discover that their favorite artist is the indie musician “Long Afternoon.” They gradually grow closer and so the story began. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem neque odio, impedit nostrum illo consequuntur! Exercitationem pariatur repudiandae, nobis sapiente fugiat architecto animi illo amet at optio natus sint eligendi.
+                        </p>
+                        <div class="btns">
+                            <a href="/scan/3" class="btn">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                                    <path d="M240-280h280v-80H240v80Zm400 0h80v-400h-80v400ZM240-440h280v-80H240v80Zm0-160h280v-80H240v80Zm-80 480q-33 0-56.5-23.5T80-200v-560q0-33 23.5-56.5T160-840h640q33 0 56.5 23.5T880-760v560q0 33-23.5 56.5T800-120H160Z" />
+                                </svg>
+                                See more
+                            </a>
+                            <a href="/scan/3/1" class="btn btn__secondary">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                                    <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm80-160h280v-80H280v80Zm0-160h400v-80H280v80Zm0-160h400v-80H280v80Z" />
+                                </svg>
+                                Read now
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div> -->
+
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 </body>
 
